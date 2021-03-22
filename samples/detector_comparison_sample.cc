f7a58487b5e960894b1be9365d522e08bae64d60
Eugene Zemskov <eugenez@sentientvision.com>
2021-03-22 13:47:32 +1100

    detector_comparison_sample : added range bounds within frame

--- samples/detector_comparison_sample.cc
+++ samples/detector_comparison_sample.cc
@@ -3,6 +3,7 @@
 #include <vector>
 #include <iostream>
 #include <iomanip>
+#include <algorithm>
 
 #include <coav/coav.hh>
 
@@ -20,6 +21,27 @@ o.id <<
         "]" << std::endl;
 }
 
+std::vector<uint16_t> GetRawDepths(const rs2::depth_frame& df)
+{
+   const size_t pointQnty = df.get_data_size() / sizeof(uint16_t);
+   const uint16_t* first(reinterpret_cast<const uint16_t*>(df.get_data()));
+   const uint16_t* last = first + pointQnty;
+
+   std::vector<uint16_t> res(pointQnty, 0);
+   std::copy(first, last, res.begin());
+   return res;
+}
+
+std::pair<uint16_t, uint16_t> GetRawDepthBounds(const std::vector<uint16_t>& rawDepthVec_)
+{
+    std::vector<uint16_t> rawDepthVec = rawDepthVec_;
+    auto itEndNonzero = std::remove(rawDepthVec.begin(), rawDepthVec.end(), 0);
+    rawDepthVec.erase(itEndNonzero, rawDepthVec.end());
+
+    const auto [minDepthRaw, maxDepthRaw] = std::minmax_element(rawDepthVec.begin(), rawDepthVec.end());
+    return std::pair<uint16_t, uint16_t>(*minDepthRaw, *maxDepthRaw);
+}
+
 int main(int argc, char **argv)
 {
     rs2::pipeline pipeline;
@@ -34,6 +56,12 @@ int main(int argc, char **argv)
     {
         rs2::frameset frames = pipeline.wait_for_frames();
         rs2::depth_frame frame = frames.get_depth_frame();
+
+        const auto depthBounds = GetRawDepthBounds(GetRawDepths(frame));
+        const float minDepth = depthBounds.first * frame.get_units(); 
+        const float maxDepth = depthBounds.second * frame.get_units(); 
+        std::cout << std::setprecision(3) << "R bounds [" << minDepth << "~" << maxDepth << "] m" << std::endl;
+
         depth_camera->set_depth_frame(&frame);
         const auto depthData = depth_camera->read();
 
